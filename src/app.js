4d959a98a65bafb999e37615bb296535aa05d208
kiruthik <kiruthik.j@kalvium.community>
2023-10-16 11:19:57 +0530

    finished

--- src/app.js
+++ src/app.js
@@ -26,6 +26,20 @@ const initPlayers = (players) => {
     let detailedPlayers = [];
     // Create players using for loop
     // Type your code here
+    for(let i = 0;i<players.length;i++){
+        if(i%2==0){
+            var play="hero"
+        }else{
+            play="villain"
+        }
+        let player ={
+            name:players[i],
+            strength:getRandomStrength(),
+            image:`images/super-${i + 1}.png`,
+            type:play
+        };
+        detailedPlayers.push(player);
+    }
 
     return detailedPlayers;
 }
@@ -34,6 +48,7 @@ const initPlayers = (players) => {
 const getRandomStrength = () => {
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
+    return Math.floor(Math.random()*100);
 }
 
 const buildPlayers = (players, type) => {
@@ -42,6 +57,17 @@ const buildPlayers = (players, type) => {
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
     // Type your code here
+    for(let i=0;i<players.length;i++){
+        if(players[i].type === type){
+            fragment += `
+               <div class="player">
+                  <img src="${players[i].image}"alt=" ">
+                  <div class ="name">${players[i].name}</div>
+                  <div class="strength">${players[i].strength}</div>
+                </div>`;
+
+        }
+    }
 
     return fragment;
 }
